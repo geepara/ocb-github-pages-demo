f773329aec941601daf3519490bd392559b82a25
geepara <62688021+geepara@users.noreply.github.com>
2023-10-31 19:34:52 -0400

    config stuff

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const withMarkdoc = require("@markdoc/next.js");
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withMarkdoc(/* options */)({
-  pageExtensions: ["md", "mdoc", "js", "jsx", "ts", "tsx"],
-});
+const nextConfig = withContentlayer(
+  withMarkdoc(/* options */)({
+    pageExtensions: ["md", "mdoc", "js", "jsx", "ts", "tsx"],
+  })
+);
 
 module.exports = nextConfig;
